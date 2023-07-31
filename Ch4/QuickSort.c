07bf855f434844cc80d6f705be21b1de6e78f05a
Ibrahim Tarek <ebrahimtarek555@gmail.com>
2023-07-31 14:41:58 +0300

    Fix Quick Sort algorithm file.

--- Ch4/QuickSort.c
+++ Ch4/QuickSort.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /*Functions' prototype*/
-void QuickSort(int NumbersArr[], int ArrSize);
+void QuickSort(int NumbersArr[], int LowIndex, int HighIndex);
 
 /*It is the main*/
 int main()
@@ -20,7 +20,7 @@ int main()
 	}
 		
 	/*Sort the array*/
-	QuickSort(NumbersArr, 20);
+	QuickSort(NumbersArr, 0, 19);
 	
 	/*Print the array elements after sorting*/
 	for(int i = 0; i < 20; i++)
@@ -38,10 +38,40 @@ void QuickSort(int NumbersArr[], int LowIndex, int HighIndex)
 	/*Define Variables*/
 	int PivotIndex = HighIndex;
 	int Pivot = NumbersArr[HighIndex];
+	int Temp;
 	
-	/**/
-	
-	/*Recursive Case*/
-	QuickSort(NumbersArr, LowIndex, PivotIndex - 1);
-	QuickSort(NumbersArr, PivotIndex + 1, HighIndex);
+	/*If the sub array is still has more than one element*/
+	if(LowIndex < HighIndex)
+	{
+		/*Define i which will carry the new index of pivot*/		
+		int i = LowIndex;
+		
+		/*Pass over the sub array*/
+		for(int j = LowIndex; j < HighIndex; j++)
+		{
+			/*move all elements that are lower than the pivot to the left of the pivot*/
+			if(NumbersArr[j] <= Pivot)
+			{
+				/*Swap between element at i with element at j*/
+				Temp = NumbersArr[i];
+				NumbersArr[i] = NumbersArr[j];
+				NumbersArr[j] = Temp;
+				
+				/*Increment i, as i is the pointer of the left side sub array*/
+				i++;
+			}
+		}
+		
+		/*Move pivot from its old index to its new index*/
+		Temp = NumbersArr[i];
+		NumbersArr[i] = NumbersArr[PivotIndex];
+		NumbersArr[PivotIndex] = Temp;
+		
+		/*Update the pivot index to call quick sort another time*/
+		PivotIndex = i;
+		
+		/*Recursive Case*/
+		QuickSort(NumbersArr, LowIndex, PivotIndex - 1);
+		QuickSort(NumbersArr, PivotIndex + 1, HighIndex);
+	}
 }
